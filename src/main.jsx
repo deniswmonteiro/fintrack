48ca78fe439b9ebd0cf0a02f3f859c88d6a81bb1
Denis Monteiro <deniswmonteiro@gmail.com>
2026-07-12 20:16:30 -0300

    feat: configurando o react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,22 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import App from "./App.jsx";
+import Cadastro from "./pages/Cadastro";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/cadastro" element={<Cadastro />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Página inicial</h1>;
+};
+
+export default Home;
