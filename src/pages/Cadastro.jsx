8ec6854bbab3697ff168510ea305171cfa449b0a
Denis Monteiro <deniswmonteiro@gmail.com>
2026-07-13 23:28:50 -0300

    feat: adicionando o campo de senha com mudança de visibilidade do valor

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,3 +1,5 @@
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import React from "react";
 import { Link } from "react-router";
 
 import { Button, buttonVariants } from "@/components/ui/button";
@@ -12,6 +14,8 @@ import {
 import { Input } from "@/components/ui/input";
 
 const Login = () => {
+  const [passwordIsVisible, setPasswordIsVisible] = React.useState(false);
+
   return (
     <section className="flex h-screen w-screen flex-col items-center justify-center gap-6">
       <Card className="w-125">
@@ -24,7 +28,21 @@ const Login = () => {
             <Input type="text" placeholder="Digite o seu nome" />
             <Input type="text" placeholder="Digite o seu sobrenome" />
             <Input type="email" placeholder="Digite o seu e-mail" />
-            <Input type="password" placeholder="Digite a sua senha" />
+            <div className="relative">
+              <Input
+                type={passwordIsVisible ? "text" : "password"}
+                placeholder="Digite a sua senha"
+                className="pr-12"
+              />
+              <Button
+                type="button"
+                variant="ghost"
+                className="text-muted-foreground absolute top-0 right-0 bottom-0 my-auto h-10 w-10"
+                onClick={() => setPasswordIsVisible((prev) => !prev)}
+              >
+                {passwordIsVisible ? <EyeOffIcon /> : <EyeIcon />}
+              </Button>
+            </div>
           </form>
         </CardContent>
         <CardFooter>
