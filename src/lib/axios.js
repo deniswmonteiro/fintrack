86cc4548b5fc522365912febd419159360473f53
Denis Monteiro <deniswmonteiro@gmail.com>
2026-08-15 19:57:09 -0300

    feat: garantindo que o token é anexado em todas as requisições feitas com a API

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+import { LOCAL_STORAGE_ACCESS_TOKEN } from "@/constants/localstorage";
+
 export const api = axios.create({
   baseURL: "https://fullstackclub-finance-dashboard-api.onrender.com/api",
+});
+
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN);
+
+  if (!accessToken) return request;
+
+  request.headers.Authorization = `Bearer ${accessToken}`;
+
+  return request;
 });
